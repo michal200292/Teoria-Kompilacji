f0e9194f069f157dde0494a31e0d72ecd9599d87
Jakub Kasperski <k2002kuba@gmail.com>
2023-11-04 17:54:59 +0100

    added requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import scanner
 
 # https://home.agh.edu.pl/~mkuta/tklab/
+# pip3 freeze > requirements.txt
+
 if __name__ == '__main__':
     filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
     try:
